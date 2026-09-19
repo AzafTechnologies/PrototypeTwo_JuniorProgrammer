65e990b3f49e3fe2e700f19e38b4cb2a7c3f2264
AzafTechnologies <azafsathar0011@gmail.com>
2026-09-19 20:12:34 +0530

    Add movement input handling
    Enable the move input action in PlayerController and read the current movement vector each frame. This also updates scene object positions in Prototype 2 to reflect the new gameplay placement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,17 @@ using UnityEngine.InputSystem;
 public class PlayerController : MonoBehaviour
 {
     public InputAction moveAction;
+
+    public Vector2 moveInput;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        moveAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveInput = moveAction.ReadValue<Vector2>();
     }
 }
