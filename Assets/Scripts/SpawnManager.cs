0bfa973220cbb3154afece651d73eab8617d64b5
AzafTechnologies <azafsathar0011@gmail.com>
2026-09-21 15:29:05 +0530

    Enable spawnAction and spawn animal on trigger
    Start now enables the input action (spawnAction). Update checks spawnAction.triggered and instantiates the first animal prefab at (0,0,20) using its prefab rotation. This wires up input to spawn behavior; assumes spawnAction and animalPrefabs are assigned in the inspector.

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,12 +8,15 @@ public class SpawnManager : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        spawnAction.Enable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (spawnAction.triggered)
+        {
+            Instantiate(animalPrefabs[0], new Vector3(0, 0, 20), animalPrefabs[0].transform.rotation);
+        }
     }
 }
